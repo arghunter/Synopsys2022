13d20234708f0e6ecd5d7df9dd71f42758d53863
Samuel Yuan <49769610+sdkyuanpanda@users.noreply.github.com>
2022-08-24 20:16:54 -0700

    Update fire-sim-2.py

--- fire-sim-2.py
+++ fire-sim-2.py
@@ -1,74 +1,108 @@
+from math import*
 import numpy as np
+import random
 import matplotlib.pyplot as plt
 from matplotlib import animation
-from matplotlib import colors
+import matplotlib as mpl
+#création de la forêt
+'''0=humus ; 1=arbre vivant ; 3=flammes ; 2=cendres'''
 
-# Create a forest fire animation based on a simple cellular automaton model.
-# The maths behind this code is described in the scipython blog article
-# at https://scipython.com/blog/the-forest-fire-model/
-# Christian Hill, January 2016.
-# Updated January 2020.
+#definition des paramètres
+a=400         #hauteur de l'image en nb de pixels
+b=400      #largeur de l'image
+density=0.65   #densité de la forêt
+i=200          #ordonnée du départ du feu
+j=300       #abscisse du départ du feu
+p=0.5          #percolation
+v=100
+w=100
 
-# Displacements from a cell to its eight nearest neighbours
-neighbourhood = ((-1,-1), (-1,0), (-1,1), (0,-1), (0, 1), (1,-1), (1,0), (1,1))
-EMPTY, TREE, FIRE = 0, 1, 2
-# Colours for visualization: brown for EMPTY, dark green for TREE and orange
-# for FIRE. Note that for the colormap to work, this list and the bounds list
-# must be one larger than the number of different values in the array.
-colors_list = [(0.2,0,0), (0,0.5,0), (1,0,0), 'orange']
-cmap = colors.ListedColormap(colors_list)
-bounds = [0,1,2,3]
-norm = colors.BoundaryNorm(bounds, cmap.N)
+def creerforet(n,p,d):  
+    D=[]
+    k=0
+    while k<1000:
+        if k<d*1000:
+            D.append(1)
+            k+=1
+        else:
+            D.append(0)
+            k+=1
+        
+    forest=np.zeros((n,p))
+    for y in range(n):
+        for x in range(p):
+            forest[y,x]=D[random.randint(0,999)]
+    return forest
+    
+    
+foret=creerforet(a,b,density)
 
-def iterate(X):
-    """Iterate the forest according to the forest-fire rules."""
+foret[i,j]=3
+#foret[v,w]=3
 
-    # The boundary of the forest is always empty, so only consider cells
-    # indexed from 1 to nx-2, 1 to ny-2
-    X1 = np.zeros((ny, nx))
-    for ix in range(1,nx-1):
-        for iy in range(1,ny-1):
-            if X[iy,ix] == EMPTY and np.random.random() <= p:
-                X1[iy,ix] = TREE
-            if X[iy,ix] == TREE:
-                X1[iy,ix] = TREE
-                for dx,dy in neighbourhood:
-                    # The diagonally-adjacent trees are further away, so
-                    # only catch fire with a reduced probability:
-                    if abs(dx) == abs(dy) and np.random.random() < 0.573:
-                        continue
-                    if X[iy+dy,ix+dx] == FIRE:
-                        X1[iy,ix] = FIRE
-                        break
-                else:
-                    if np.random.random() <= f:
-                        X1[iy,ix] = FIRE
-    return X1
+fig = plt.figure(figsize=(15,15))
 
-# The initial fraction of the forest occupied by trees.
-forest_fraction = 0.2
-# Probability of new tree growth per empty cell, and of lightning strike.
-p, f = 0.05, 0.0001
-# Forest size (number of cells in x and y directions).
-nx, ny = 100, 100
-# Initialize the forest grid.
-X  = np.zeros((ny, nx))
-X[1:ny-1, 1:nx-1] = np.random.randint(0, 2, size=(ny-2, nx-2))
-X[1:ny-1, 1:nx-1] = np.random.random(size=(ny-2, nx-2)) < forest_fraction
 
-fig = plt.figure(figsize=(25/3, 6.25))
-ax = fig.add_subplot(111)
-ax.set_axis_off()
-im = ax.imshow(X, cmap=cmap, norm=norm)#, interpolation='nearest')
+def step(): 
+    Feu=[]  #liste contenant toutes les coordonnees des arbres en train de bruler
+    for y in range(a):
+        for z in range(b):
+            if foret[y][z]==3:
+                Feu.append([y,z])
+    Perc=[]
+    l=0
+    while l<1000:
+        if l<p*1000:
+            Perc.append(3)
+            l+=1
+        else:
+            Perc.append(1)
+            l+=1
+    for x in range(len(Feu)):
+        r=Feu[x][0]
+        s=Feu[x][1]
 
-# The animation function: called to produce a frame for each generation.
-def animate(i):
-    im.set_data(animate.X)
-    animate.X = iterate(animate.X)
-# Bind our grid to the identifier X in the animate function's namespace.
-animate.X = X
+        if foret[r+1,s]==1:
+            foret[r+1,s]=Perc[random.randint(0,999)]
+        if foret[r+1,s-1]==1:
+            foret[r+1,s-1]=Perc[random.randint(0,999)]
+        if foret[r+1,s+1]==1:
+            foret[r+1,s+1]=Perc[random.randint(0,999)]
 
-# Interval between frames (ms).
-interval = 100
-anim = animation.FuncAnimation(fig, animate, interval=interval, frames=200)
+        if foret[r-1,s-1]==1:
+            foret[r-1,s-1]=Perc[random.randint(0,999)]
+        if foret[r-1,s]==1:
+            foret[r-1,s]=Perc[random.randint(0,999)]
+        if foret[r-1,s-1]==1:
+            foret[r-1,s-1]=Perc[random.randint(0,999)]
+
+        if foret[r,s+1]==1:
+            foret[r,s+1]=Perc[random.randint(0,999)]
+        if foret[r,s-1]==1:
+            foret[r,s-1]=Perc[random.randint(0,999)]
+
+    #reduction en cendres des arbres brulés à l'étape précédente        
+    for y in range(len(Feu)):
+        foret[Feu[y][0]][Feu[y][1]]=2
+    return foret
+        
+CM = mpl.colors.ListedColormap([[0.29,0.01,0,0.6],[0.156,0.59,0,1],[0.1,0.1,0.1,1], [0.87,0.3,0.2,0.9]])
+im = plt.imshow(step(), cmap=CM, interpolation='none')
+
+
+ 
+def updatefig(*args):
+    im.set_array(step())
+    return im,
+ 
+ 
+
+ani = animation.FuncAnimation(fig, updatefig, interval=10, blit=True)
+# anim.save can be called in a few different ways, some which might or might not work
+# on different platforms and with different versions of matplotlib and video encoders
+#anim.save('animation.mp4', fps=20, extra_args=['-vcodec', 'libx264'], writer=animation.FFMpegWriter())
+#anim.save('animation.mp4', fps=20, extra_args=['-vcodec', 'libx264'])
+#anim.save('animation.mp4', fps=20, writer="ffmpeg", codec="libx264")
+#anim.save('animation.mp4', fps=20, writer="avconv", codec="libx264")
+plt.axis('off')
 plt.show()
