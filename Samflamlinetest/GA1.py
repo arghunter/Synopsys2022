ec5768debf5d278ed475312307c2805ee63bd60b
Samuel Yuan <49769610+sdkyuanpanda@users.noreply.github.com>
2022-09-09 10:48:29 -0700

    Create GA1.py

---
+++ Samflamlinetest/GA1.py
@@ -0,0 +1,31 @@
+import time
+from tkinter.tix import Tree
+import numpy as np
+import matplotlib.pyplot as plt
+import math
+from matplotlib import animation
+from matplotlib import colors
+from queue import Queue
+import threading
+
+
+# import files
+from userInfo import userInfo
+
+maxGenerations = input("Number of Generations: ")
+intmaxGenerations = int(maxGenerations)
+
+
+generationBig = []
+
+if len(generationBig) <= intmaxGenerations:
+    generationBig.append(1)
+    print("GENERATION: ", len(generationBig), "OUT OF", intmaxGenerations)
+    import flamlineGA
+
+else:
+    print("complete")
+
+
+
+
