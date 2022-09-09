3085dc4101e6f15913e9d70c6a366a4e5c2ad4ee
arghunter <agomes673@student.fuhsd.org>
2022-09-08 21:37:10 -0700

    xy genome

--- flamlineGA/genome.py
+++ flamlineGA/genome.py
@@ -3,8 +3,8 @@ import numpy as np
 
 class Genome:
     nV = 3
-    v = np.zeros(nV)
+    v = np.zeros((nV, 2))
 
     def __init__(self, nV):
         self.nV = nV
-        self.v = np.zeros(nV)
+        self.v = np.zeros((nV, 2))
