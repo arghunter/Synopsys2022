e0a07b14ad0fa807789e37389d03418c95d24be8
arghunter <agomes673@student.fuhsd.org>
2022-09-16 15:30:01 -0700

    Update genome.py

--- flamlineGA/genome.py
+++ flamlineGA/genome.py
@@ -11,15 +11,17 @@ class Genome:
         self.nV = int(nV)
         print("ere")
         self.v = np.zeros((self.nV, 2))
-        # r = sideLength/2.0
-        r = 30
+        r = sideLength/2.0
+        # r = 30
 
         qoffset = int(np.random.random()*self.nV)
-        rant = np.pi*2/self.nV
+        rant = np.pi/self.nV
         for i in range(self.nV):
             while True:
                 angle = ((np.pi*2)/self.nV)*(i+qoffset) + \
                     (np.random.random()-0.5)*rant
+                mag = (3*r/4)/np.cos((np.abs(angle*180/np.pi) % 90))+r/4
+
                 mag = r/np.cos((np.abs(angle*180/np.pi) % 90))
                 self.v[i][0] = centery + mag*np.sin(angle*180/np.pi)
                 self.v[i][1] = centerx + mag*np.cos(angle*180/np.pi)
@@ -31,6 +33,6 @@ class Genome:
     #     pass
 
 
-gnme = Genome(4)
-for i in range(4):
-    print(str(gnme.v[i][0])+" "+str(gnme.v[i][1]))
+gnme = Genome(10)
+for i in range(10):
+    print(str(gnme.v[i][0])+" "+str(gnme.v[i][1])+")")
