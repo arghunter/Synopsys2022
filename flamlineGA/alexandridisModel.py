1383a272f7240693b49339c65c71016fe839183a
Samuel Yuan <49769610+sdkyuanpanda@users.noreply.github.com>
2022-10-07 09:53:08 -0700

    working alexandridis model

--- flamlineGA/alexandridisModel.py
+++ flamlineGA/alexandridisModel.py
@@ -2,7 +2,7 @@
 from userInfo import *
 import math
 
-ph = 0.58 # pre optimised
+ph = 0.58  # pre optimised
 
 # p veg dependent on land type - pre defined by alexandridis
 if landType == 1:
@@ -28,23 +28,21 @@ U = 0
 thetaw = 45
 
 
-
 def alexandridisModelProbability(thetaS, thetaf):
     # wind direction relative to fire spread direction - thetawf
     thetawf = thetaw - thetaf
-    
+
     # slope influenced probability
-    ps = math.exp(a*thetaS)
+    ps = math.exp(a * thetaS)
 
     # wind influenced probability parameter
-    fsubt = math.exp(U * c2 * ((math.cos(thetawf))-1))
+    fsubt = math.exp(U * c2 * ((math.cos(thetawf)) - 1))
 
     # wind influenced probability
     pw = (fsubt * math.exp(c1 * U))
 
     # burn probability
-    pburn = ph * (1 + pveg) * pw * ps
+    pburn = (ph * (1 + pveg) * pw * ps)
 
-    spread_chance = pburn
-    return spread_chance
+    return pburn
 
