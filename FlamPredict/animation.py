a554ca691291a2ba1a7f551d53b530e759f320ce
arghunter <acgo8888@gmail.com>
2022-11-06 17:43:49 -0800

    Revert "fire count"
    This reverts commit 7e08665af2abed89a4ca1fe5796d957e932220f1.

--- FlamPredict/animation.py
+++ FlamPredict/animation.py
@@ -21,11 +21,8 @@ ax = fig.add_subplot(111)
 # ax.set_axis_off()
 colors_list = [(0,0.4,0),(0,0,1)]
 bounds = [0.0,1.0]
-count=0
-for i,j in range(X.shape()):
-   if(X[i][j]!=0):
-      count+=1
-simulateTime =1440*1000
+
+simulateTime =1440
 for i in range(2,simulateTime,120):
    colors_list.append(((i)/(simulateTime),0,0))
    bounds.append(i)
