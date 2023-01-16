16a8d68f66c5321ac0524dec1728469bbb58b3dd
Samuel Yuan <49769610+sdkyuanpanda@users.noreply.github.com>
2023-01-16 11:47:52 -0800

    save time

--- FlamPredict/alexandridisModel.py
+++ FlamPredict/alexandridisModel.py
@@ -3,42 +3,17 @@
 import math
 
 
-with open('alexandridisVar.txt', 'r') as varFile:
-    lines = varFile.readlines()
+p0 = 0.7
+c1 = 0.045
+c2 = 0.191
 
-    second_line = lines[(2 - 1)]
-    second_line = second_line.strip()
-
-    fifth_line = lines[(5 - 1)]
-    fifth_line = fifth_line.strip()
-
-    eighth_line = lines[(8 - 1)]
-    eighth_line = eighth_line.strip()
-
-    eleventh_line = lines[(11 - 1)]
-    eleventh_line = eleventh_line.strip()
-
-    fourteenth_line = lines[(14 - 1)]
-    fourteenth_line = fourteenth_line.strip()
-
-    seventeenth_line = lines[(17 - 1)]
-    seventeenth_line = seventeenth_line.strip()
-
-    twentyfirst_line = lines[(21 - 1)]
-    twentyfirst_line = twentyfirst_line.strip()
-
-
-p0 = float(second_line)
-c1 = float(fifth_line)
-c2 = float(eighth_line)
-
-a = float(eleventh_line)
-b = float(fourteenth_line)
-d = float(seventeenth_line)
+a = 2.5
+b = 0.15
+d = 0.932
 
 
 # altitude factor
-asubs = float(twentyfirst_line)
+asubs = 0.088
 
 def alexandridisModelProbability(thetaf, thetaw, mgw, pveg, pden, h, rthetaS, Mx):
     # calculation of p0
@@ -70,15 +45,15 @@ def alexandridisModelProbability(thetaf, thetaw, mgw, pveg, pden, h, rthetaS, Mx
     # print("wind influenced prob", pw)
 
     # moisture content: data link: https://www.wfas.net/index.php/national-fuel-moisture-database-moisture-drought-103
-    Cm = 6
+    Cm = 9
     MxP = Mx * 100
     #
     # threshold with extinction fuel moisture
     if MxP <= Cm:
-          pm = 0
+        pm = 0
     else:
-          # moisture probability
-          pm = a * math.exp(((-1) * b * Cm))
+        # moisture probability
+        pm = a * math.exp(((-1) * b * Cm))
     #
     # height probability
     ph = (((h/3.281))**d)
